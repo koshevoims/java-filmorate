de6ce7ac243073d5e88c6f24ea6691425c637a7e
Maxim Koshevoy <mkoshevoy@rencredit.ru>
2024-02-24 12:49:17 +0300

    delete unused import

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,19 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/films")
@@ -52,7 +45,6 @@ public class FilmController {
             return film;
         }
         return film;
-
     }
 
     private Boolean validCheck(Film film) {
