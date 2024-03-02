544b8912af188de8ed156203b582e7c4c1fb440d
Maxim Koshevoy <mkoshevoy@rencredit.ru>
2024-03-02 17:10:19 +0300

    Add controllers validation and tests

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,42 +4,50 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
-    private final HashMap<Integer, Film> films = new HashMap<>();
+    private final Map<Integer, Film> films = new HashMap<>();
     private Integer id = 0;
 
     @GetMapping()
     public List<Film> getAll() {
-        log.info("Получили запрос. Количество фильмов: " + films.size());
+        log.info("Получили запрос. Количество фильмов: {}", films.size());
         return List.copyOf(films.values());
     }
 
+    @GetMapping("/films/{id}")
+    public Film getId(@PathVariable Integer id) {
+        log.info("Получили запрос. Фильм id: {}", id);
+        return films.get(id);
+    }
+
     @PostMapping()
-    public Film create(@RequestBody Film film) {
+    public Film create(@Valid @RequestBody Film film) {
         if (validCheck(film)) {
-            id++;
-            film.setId(id);
-            log.info("Сохраняем фильм: " + film.toString());
+            film.setId(++id);
+            log.info("Сохраняем фильм: {}", film.toString());
             films.put(film.getId(), film);
             return film;
         } else return film;
     }
 
     @PutMapping()
-    public Film put(@RequestBody Film film) {
+    public Film put(@Valid @RequestBody Film film) {
         if (validCheck(film)) {
             if (films.containsKey(film.getId())) {
-                log.info("Обновляем фильм: " + film.toString());
+                log.info("Обновляем фильм: {}", film.toString());
                 films.put(film.getId(), film);
             } else {
-                log.info("Фильм не существует: " + film.toString());
+                log.error("Фильм не существует: {}", film.toString());
                 throw new ValidationException("Фильма не существует не существует");
             }
             return film;
@@ -48,21 +56,9 @@ public class FilmController {
     }
 
     private Boolean validCheck(Film film) {
-        if (film.getName().isBlank()) {
-            log.info("название не может быть пустым: " + film.toString());
-            throw new ValidationException("название не может быть пустым");
-        }
-        if (film.getDescription().length() >= 200) {
-            log.info("максимальная длина описания — 200 символов: " + film.toString());
-            throw new ValidationException("максимальная длина описания — 200 символов");
-        }
         if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
-            log.info("дата релиза — не раньше 28 декабря 1895 года: " + film.toString());
+            log.error("дата релиза — не раньше 28 декабря 1895 года: {}", film.toString());
             throw new ValidationException("дата релиза — не раньше 28 декабря 1895 года");
-        }
-        if (film.getDuration() <= 0) {
-            log.info("продолжительность фильма должна быть положительной: " + film.toString());
-            throw new ValidationException("продолжительность фильма должна быть положительной");
         }
         return true;
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,31 +5,37 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-    private final HashMap<Integer, User> users = new HashMap<>();
+    private final Map<Integer, User> users = new HashMap<>();
 
     private Integer id = 0;
 
     @GetMapping()
-    public List<User> getAll() {
-        log.info("Получили запрос. Количество пользователей: " + users.size());
+    public List<User> getUserAll() {
+        log.info("Получили запрос. Количество пользователей: {}", users.size());
         return List.copyOf(users.values());
+    }
 
+    @GetMapping("/users/{id}")
+    public User getUserId(@PathVariable Integer id) {
+        log.info("Получили запрос. Пользователь id: {}", id);
+        return users.get(id);
     }
 
     @PostMapping()
-    public User create(@RequestBody User user) {
+    public User postUser(@Valid @RequestBody User user) {
         if (validCheck(user)) {
-            id++;
-            user.setId(id);
-            log.info("Сохраняем пользователя: " + user.toString());
+            user.setId(++id);
+            log.info("Сохраняем пользователя: {}", user.toString());
             users.put(user.getId(), user);
             return user;
         }
@@ -37,13 +43,13 @@ public class UserController {
     }
 
     @PutMapping()
-    public User put(@RequestBody User user) {
+    public User putUser(@Valid @RequestBody User user) {
         if (validCheck(user)) {
             if (users.containsKey(user.getId())) {
-                log.info("Обновляем пользователя: " + user.toString());
+                log.info("Обновляем пользователя: {}", user.toString());
                 users.put(user.getId(), user);
             } else {
-                log.info("Пользователя не существует: " + user.toString());
+                log.error("Пользователя не существует: {}", user.toString());
                 throw new ValidationException("Пользователя не существует");
             }
             return user;
@@ -52,28 +58,12 @@ public class UserController {
     }
 
     private Boolean validCheck(User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            log.info("электронная почта не может быть пустой: " + user.toString());
-            throw new ValidationException("электронная почта не может быть пустой");
-        }
-        if (!user.getEmail().contains("@")) {
-            log.info("электронная почта должна содержать символ @: " + user.toString());
-            throw new ValidationException("электронная почта должна содержать символ @");
-        }
-        if (user.getLogin().isBlank()) {
-            log.info("логин не может быть пустым: " + user.toString());
-            throw new ValidationException("логин не может быть пустым");
-        }
         if (user.getLogin().contains(" ")) {
-            log.info("логин не может содержать пробелы: " + user.toString());
+            log.error("логин не может содержать пробелы: {}", user.toString());
             throw new ValidationException("логин не может содержать пробелы");
         }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.info("дата рождения не может быть в будущем: " + user.toString());
-            throw new ValidationException("дата рождения не может быть в будущем");
-        }
         if (user.getName() == null || user.getName().isBlank()) {
-            log.info("имя для отображения может быть пустым — в таком случае будет использован логин: " + user.toString());
+            log.info("имя для отображения может быть пустым — в таком случае будет использован логин: {}", user.toString());
             user.setName(user.getLogin());
         }
         return true;
